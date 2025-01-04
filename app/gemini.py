1c03df5c7db08b7c094c68c17097d1c91c7c1ee6
Gavin <gavin.liu2011@gmail.com>
2025-01-04 15:30:24 -0800

    Play around

--- app/gemini.py
+++ app/gemini.py
@@ -1,9 +1,19 @@
 import vertexai
-from vertexai.generative_models import GenerativeModel
-from floggit import flog
+from vertexai.language_models import GenerativeModel
+import logging
+
+logging.basicConfig(level=logging.INFO)
+def flog(func):
+    def wrapper(*args, **kwargs):
+        logging.info(f"Calling function {func.__name__} with arguments {args} and {kwargs}")
+        result = func(*args, **kwargs)
+        logging.info(f"Function {func.__name__} returned {result}")
+        return result
+    return wrapper
 
 PROJECT_ID = "learned-grammar-426001-g1"
 vertexai.init(project=PROJECT_ID, location="us-west1")
+
 
 model = GenerativeModel("gemini-1.5-flash-002")
 @flog
--- app/main.py
+++ app/main.py
@@ -2,14 +2,19 @@ from floggit import flog
 import streamlit as st
 from gemini import generate_content
 
-if 'chat_history' not in st.session_state:
-    st.session_state.chat_history = []
-
+st.header("RojosBrother's Chatbot")
+st.write("This is a chatbot that generates responses based on the input you provide.")
 @flog
 def update_history():
     st.session_state.chat_history.append(st.session_state.chat_input)
     gemini_response = generate_content(st.session_state.chat_history)
     st.session_state.chat_history.append(gemini_response)
 
-st.write(st.session_state.chat_history)
+
+
+if 'chat_history' not in st.session_state:
+    st.session_state.chat_history = []
+
+if len(st.session_state.chat_history):
+    st.write(st.session_state.chat_history[-1])
 st.chat_input(on_submit=update_history, key='chat_input')
